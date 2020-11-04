fad8b45247c0d942e3bb2f833b5fcec938fcd4d1
zheta88 <zytamayo@misena.edu.co>
2020-11-04 13:46:49 -0500

    repo local

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin\source\repos\LocatorEnel\LocatorEnel\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
