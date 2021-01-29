f963a370b6207101d2571408113d6b60b85c03ee
Zully Tamayo M <52215922+zheta88@users.noreply.github.com>
2021-01-29 11:39:16 -0500

    Delete Scripts directory

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Admin\source\repos\LocatorEnel\LocatorEnel\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
